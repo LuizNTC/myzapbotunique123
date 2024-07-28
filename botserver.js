2eb113db695a8d6f083a269263c3608c924d2b74
LuizNTC <luizgustavofmachado@gmail.com>
2024-07-28 16:40:50 -0300

    Ensure all files are correct

--- botserver.js
+++ botserver.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { create } = require('venom-bot');
 const path = require('path');
 const WebSocket = require('ws');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
